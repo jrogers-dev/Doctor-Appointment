755778280ab85cab745e6e9e9698925f84d86630
James Rogers <jarogers095@gmail.com>
2020-09-11 16:37:56 -0700

    First commit! Generated models and controllers for appointments, doctors and patients. Roughly filled out patient index view.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,29 @@
+class AppointmentsController < ApplicationController
+    def index
+        @appointments = Appointment.all
+    end
+
+
+    def new
+
+    end
+
+
+    def create
+
+    end
+
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+
+    def delete
+
+    end
+end
